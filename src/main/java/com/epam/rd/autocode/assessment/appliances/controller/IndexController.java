b3901d26b021ea204dca999ff230e624f886b8e7
witchkaa <victoriasabadan1@gmail.com>
2025-06-24 21:43:53 +0300

    Add locales for index

--- src/main/java/com/epam/rd/autocode/assessment/appliances/controller/IndexController.java
+++ src/main/java/com/epam/rd/autocode/assessment/appliances/controller/IndexController.java
@@ -3,11 +3,14 @@ package com.epam.rd.autocode.assessment.appliances.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Locale;
+
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Locale locale) {
+        System.out.println(">> Current locale: " + locale);
         return "index"; // resources/templates/index.html
     }
 }
