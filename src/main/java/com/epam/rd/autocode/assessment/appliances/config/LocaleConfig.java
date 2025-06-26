19f17048cac429a7234fb78f34222e6fd3c45631
witchkaa <victoriasabadan1@gmail.com>
2025-06-26 03:00:23 +0300

    Fix localization

--- src/main/java/com/epam/rd/autocode/assessment/appliances/config/LocaleConfig.java
+++ src/main/java/com/epam/rd/autocode/assessment/appliances/config/LocaleConfig.java
@@ -1,13 +1,14 @@
 package com.epam.rd.autocode.assessment.appliances.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
@@ -16,14 +17,23 @@ public class LocaleConfig implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.ENGLISH);
-        return slr;
+        CookieLocaleResolver clr = new CookieLocaleResolver();
+        clr.setDefaultLocale(Locale.ENGLISH);
+        clr.setCookieName("lang");
+        clr.setCookieMaxAge(3600 * 24 * 30);
+        return clr;
+    }
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("messages/messages");
+        source.setDefaultEncoding("UTF-8");
+        return source;
     }
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
         LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-        lci.setParamName("lang");
+        lci.setParamName("lang"); // ?lang=uk / ?lang=en
         return lci;
     }
 
