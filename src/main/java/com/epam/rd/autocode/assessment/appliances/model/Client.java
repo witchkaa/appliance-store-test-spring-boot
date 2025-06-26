defad22478f439d881a228439e598ef14939765d
witchkaa <victoriasabadan1@gmail.com>
2025-06-26 03:42:00 +0300

    Add card format validation

--- src/main/java/com/epam/rd/autocode/assessment/appliances/model/Client.java
+++ src/main/java/com/epam/rd/autocode/assessment/appliances/model/Client.java
@@ -2,6 +2,7 @@ package com.epam.rd.autocode.assessment.appliances.model;
 
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -12,6 +13,7 @@ import lombok.*;
 @AllArgsConstructor
 public class Client extends User {
     @NotBlank(message = "{client.card.notblank}")
-    @Size(min = 8, max = 20, message = "{client.card.size}")
+    @Size(min = 9, max = 9, message = "{client.card.size}")
+    @Pattern(regexp = "^\\d{4}-\\d{4}$", message = "{client.card.pattern}")
     private String card;
 }
