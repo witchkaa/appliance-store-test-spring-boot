a3de581cb0891547014ee8300c91174132ac2256
witchkaa <victoriasabadan1@gmail.com>
2025-07-05 20:42:47 +0300

    Add ssl cert to repo

--- src/main/java/com/epam/rd/autocode/assessment/appliances/config/SecurityConfig.java
+++ src/main/java/com/epam/rd/autocode/assessment/appliances/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                //.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/reset/**").permitAll()
